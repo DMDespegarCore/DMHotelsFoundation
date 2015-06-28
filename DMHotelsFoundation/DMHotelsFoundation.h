0d2ce246d456e421095df506a7d5361bcfbc0a35
Matias Servetto <mservetto@despegar.com>
2015-06-28 15:45:00 -0300

    Initial Commit

---
+++ DMHotelsFoundation/DMHotelsFoundation.h
@@ -0,0 +1,19 @@
+//
+//  DMHotelsFoundation.h
+//  DMHotelsFoundation
+//
+//  Created by Matias Servetto on 6/28/15.
+//  Copyright (c) 2015 Despegar.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMHotelsFoundation.
+FOUNDATION_EXPORT double DMHotelsFoundationVersionNumber;
+
+//! Project version string for DMHotelsFoundation.
+FOUNDATION_EXPORT const unsigned char DMHotelsFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMHotelsFoundation/PublicHeader.h>
+
+
