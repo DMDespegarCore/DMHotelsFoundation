aa567f6e25a4936e96116f196ec41894e66b5116
Nicolas Miyasato <nicolas.miyasato@despegar.com>
2015-07-07 15:21:00 -0300

    Agrego clase en común.

---
+++ DMHotelsFoundation/DMInfo.h
@@ -0,0 +1,15 @@
+//
+//  DMInfo.h
+//  DMHotelsFoundation
+//
+//  Created by Nicolas Miyasato on 7/7/15.
+//  Copyright (c) 2015 Despegar.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMInfo : NSObject
+
+- (void) sayHello;
+
+@end
---
+++ DMHotelsFoundation/DMInfo.m
@@ -0,0 +1,18 @@
+//
+//  DMInfo.m
+//  DMHotelsFoundation
+//
+//  Created by Nicolas Miyasato on 7/7/15.
+//  Copyright (c) 2015 Despegar.com. All rights reserved.
+//
+
+#import "DMInfo.h"
+
+@implementation DMInfo
+
+- (void) sayHello {
+    NSLog(@"Hello from Hotels");
+}
+
+
+@end
