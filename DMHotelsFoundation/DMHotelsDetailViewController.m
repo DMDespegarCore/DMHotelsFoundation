58bbe941c6690d4e68e04bc029077d2e673a9b42
Nicolas Miyasato <nicolas.miyasato@despegar.com>
2015-07-07 15:48:58 -0300

    Agrego imagen con el mismo nombre en flights.

--- DMHotelsFoundation/DMHotelsDetailViewController.m
+++ DMHotelsFoundation/DMHotelsDetailViewController.m
@@ -9,9 +9,18 @@
 #import "DMHotelsDetailViewController.h"
 
 @interface DMHotelsDetailViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation DMHotelsDetailViewController
 
+-(void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Hay que indicarle el bundle del cual hay que buscar la imagen.
+    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+    
+    self.imageView.image = [UIImage imageNamed:@"check" inBundle:bundle compatibleWithTraitCollection:nil];
+}
 @end
